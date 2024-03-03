ba60bc3be16014f2f2874dce851193c37c7d6ea8
hector <hector.morales.carnice@gmail.com>
2024-03-03 02:39:21 +0100

    chore: use wasi?

--- embedder/src/bin/wasm.rs
+++ embedder/src/bin/wasm.rs
@@ -1,21 +1,35 @@
 use std::{fs::File, io::Read};
-use wasmer::{Store, Module, Instance, Value, imports};
+use wasmer::{imports, Instance, Module, Store};
+// use wasmer_wasix::WasiEnv;
 
 static PLUGIN_PATH: &'static str = "./target/wasm32-unknown-unknown/release/wasm_plugin.wasm";
 
-fn main() {
+fn main(){
     let mut f = File::open(PLUGIN_PATH).unwrap();
-    
     let mut wasm_plugin = Vec::default();
     f.read_to_end(&mut wasm_plugin).unwrap();
 
     let mut store = Store::default();
     let module = Module::new(&store, &wasm_plugin).unwrap();
 
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let _guard = runtime.enter();
+
+    // let mut wasi_env = WasiEnv::builder("engine").finalize(&mut store).unwrap();
+    // let import_object = wasi_env.import_object(&mut store, &module).unwrap();
     let import_object = imports! {};
     let instance = Instance::new(&mut store, &module, &import_object).unwrap();
 
-    let add = instance.exports.get_function("add").unwrap();
-    let result = add.call(&mut store, &[Value::I32(1), Value::I32(2)]).unwrap();
-    assert_eq!(result[0], Value::I32(3));
+    // wasi_env.initialize(&mut store, instance.clone()).unwrap();
+
+    let start = instance.exports.get_typed_function::<u8, u8>(&mut store, "add").unwrap();
+    let result = start.call(&mut store, 2).unwrap();
+    assert_eq!(result, 2);
+
+    let result = start.call(&mut store, 3).unwrap();
+    assert_eq!(result, 5);
 }
--- wasm-plugin/src/lib.rs
+++ wasm-plugin/src/lib.rs
@@ -1,7 +1,25 @@
+use std::sync::Mutex;
+
+static ADD_BASE: Mutex<u8> = Mutex::new(0);
+
 #[no_mangle]
-pub extern "C" fn add(left: usize, right: usize) -> usize {
-    left + right
+fn add(delta: u8) -> u8 {
+    let mut value = ADD_BASE.lock().unwrap();
+    *value += delta;
+    *value
 }
+
+// #[no_mangle]
+// pub extern "C" fn echo(ptr: u64) -> Text {
+//     let msg = unsafe {
+//          let len = *(ptr as *const u32);
+//          let bytes = (ptr + 4) as *const u8;
+//          let slice = core::slice::from_raw_parts(bytes, len as usize);
+//          core::str::from_utf8_unchecked(slice)
+//     };
+
+//     Text { ptr: 0, len: 0 }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -9,7 +27,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = add(2);
+        assert_eq!(result, 2);
+
+        let result = add(3);
+        assert_eq!(result, 5);
     }
 }
