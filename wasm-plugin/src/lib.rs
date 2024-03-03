55ef275ff3503759db0b8409ef064c61b6050ec7
hector <hector.morales.carnice@gmail.com>
2024-03-03 02:40:51 +0100

    chore: use extern C

--- wasm-plugin/src/lib.rs
+++ wasm-plugin/src/lib.rs
@@ -3,7 +3,7 @@ use std::sync::Mutex;
 static ADD_BASE: Mutex<u8> = Mutex::new(0);
 
 #[no_mangle]
-fn add(delta: u8) -> u8 {
+pub extern "C" fn add(delta: u8) -> u8 {
     let mut value = ADD_BASE.lock().unwrap();
     *value += delta;
     *value
